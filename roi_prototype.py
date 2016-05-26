c5363fefd521172e7a4bcbecfb36fb9dd3cccbea
Josh Bradshaw <joshbradshaw11@gmail.com>
2016-05-26 09:44:12 -0400

    got slice selection working

--- roi_prototype.py
+++ roi_prototype.py
@@ -16,7 +16,7 @@ import ROI
 import blood_tools
 import numpy as np
 
-def MyPyQtSlot(*args):
+def QTSlotExceptionRationalizer(*args):
     """
     for strange, mostly undocumented reasons, the default behaviour of PyQt
     is to silence all exceptions, which makes the debugging process roughly 
@@ -49,7 +49,7 @@ class ROISelectPlot(QtGui.QWidget):
      - multiple ROI selection for multiple slices
      - next/prev slice advancement with a progress indicator
     """
-    @MyPyQtSlot("bool")    
+    @QTSlotExceptionRationalizer("bool")    
     def __init__(self, parent=None):
         super(ROISelectPlot, self).__init__(parent)
         self.roi = None
@@ -57,15 +57,14 @@ class ROISelectPlot(QtGui.QWidget):
         self.axes = self.figure.add_subplot(111, autoscale_on=True)
         
         self.canvas = FigureCanvas(self.figure)
-        #self.toolbar = NavigationToolbar2QT(self.canvas, self)
-
+        self.toolbar = NavigationToolbar2QT(self.canvas, self)
         # set the layout
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.canvas)
-        #layout.addWidget(self.toolbar)
+        layout.addWidget(self.toolbar)
         self.setLayout(layout)
         
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def make_image(self, im):
         self.clear_roi()
         self.axes.clear()
@@ -76,14 +75,12 @@ class ROISelectPlot(QtGui.QWidget):
         self.figure.canvas.draw()
         self.axes.set_autoscale_on(False)
         
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def new_roi(self):
         self.roi = ROI.new_ROI(self.im, self.axes, self.figure)
         
-    @MyPyQtSlot("bool")
-    def clear_roi(self):
-        print "clear ROI called!!"        
-        
+    @QTSlotExceptionRationalizer("bool")
+    def clear_roi(self):        
         if self.roi is not None: # check if there is an ROI
             self.roi.patch.remove()
             self.roi.disconnect()
@@ -91,7 +88,7 @@ class ROISelectPlot(QtGui.QWidget):
             self.roi = None
             self.figure.canvas.draw()
             
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def get_roi(self):
         return self.roi
         
@@ -100,25 +97,32 @@ class T2CurvePlot(QtGui.QWidget):
     The plot that displays the datapoints, the fitted monoexponential T2 curve, 
     and gives the value for T2 based on this fit.
     """
-    @MyPyQtSlot("bool")    
+    @QTSlotExceptionRationalizer("bool")    
     def __init__(self, parent=None):
         super(T2CurvePlot, self).__init__(parent)
         self.roi = None
         self.figure = Figure()
         self.axes = self.figure.add_subplot(111, autoscale_on=True)
-        
         self.canvas = FigureCanvas(self.figure)
-        #self.toolbar = NavigationToolbar2QT(self.canvas, self)
-
+        self.toolbar = NavigationToolbar2QT(self.canvas, self)
         # set the layout
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self.canvas)
-        #layout.addWidget(self.toolbar)
+        layout.addWidget(self.toolbar)
         self.setLayout(layout)
         
 class MainWindow(QtGui.QWidget):
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def __init__(self):
+        # draw the interface
+        self.init_gui()
+        # initialize these to empty lists so that the slice select display will work before loading images
+        self.images, self.image_attributes = [], []    
+        # used for keeping track of what slice is displayed
+        self.image_index = 0
+
+    @QTSlotExceptionRationalizer("bool")
+    def init_gui(self):
         QtGui.QWidget.__init__(self, parent=None)
         
         button_load = QtGui.QPushButton(qtawesome.icon('fa.folder-open-o'), '')
@@ -126,11 +130,15 @@ class MainWindow(QtGui.QWidget):
         button_draw_roi = QtGui.QPushButton('Draw ROI') 
         
         button_slice_fwd = QtGui.QPushButton(qtawesome.icon('fa.chevron-right'), '')
+        button_slice_fwd.setObjectName('slice_fwd')
         button_slice_bwd = QtGui.QPushButton(qtawesome.icon('fa.chevron-left'), '')
+        button_slice_bwd.setObjectName('slice_bwd')
         
         button_slice_first = QtGui.QPushButton(qtawesome.icon('fa.step-backward'), '')
+        button_slice_first.setObjectName('slice_first')
         button_slice_last = QtGui.QPushButton(qtawesome.icon('fa.step-forward'), '')
-        self.slice_label = QtGui.QLabel('(0/0)')
+        button_slice_last.setObjectName('slice_last')
+        self.slice_label = QtGui.QLabel('(00/00)')
         
         self.combo_roi_scope = QtGui.QComboBox()   
         self.combo_roi_scope.addItems(['This Slice','All Slices'])
@@ -149,9 +157,10 @@ class MainWindow(QtGui.QWidget):
         self.plot_graph = T2CurvePlot(self)
         
         layout_top = QtGui.QHBoxLayout()
+        layout_top.addSpacing(10)
         layout_top.addWidget(button_load)
         
-        layout_top.addWidget(QtGui.QLabel('     '))
+        layout_top.addStretch()
         layout_top.addWidget(QtGui.QLabel('Change Slice:'))
 
         layout_top.addWidget(button_slice_first)
@@ -160,21 +169,21 @@ class MainWindow(QtGui.QWidget):
         layout_top.addWidget(button_slice_last)
         layout_top.addWidget(self.slice_label)
         
-        layout_top.addWidget(QtGui.QLabel('     '))
+        layout_top.addStretch()
         layout_top.addWidget(QtGui.QLabel('ROI Style:'))
         layout_top.addWidget(self.combo_roi_style)
         layout_top.addWidget(QtGui.QLabel('Apply ROI to:'))
         layout_top.addWidget(self.combo_roi_scope)
         layout_top.addWidget(button_draw_roi)
-        layout_top.addWidget(QtGui.QLabel('     '))
+        layout_top.addStretch()
         layout_top.addWidget(combo_relax_label)
         layout_top.addWidget(self.combo_relax)
         layout_top.addWidget(button_run)
-        layout_top.addStretch()
         
         layout_mid = QtGui.QHBoxLayout()
         layout_mid.addWidget(self.plot_im)
         layout_mid.addWidget(self.plot_graph)
+        layout_top.addSpacing(10)
         
         layout_main = QtGui.QVBoxLayout()
         layout_main.addLayout(layout_top)
@@ -183,21 +192,46 @@ class MainWindow(QtGui.QWidget):
         
         button_load.clicked.connect(self.choose_dir)
         button_run.clicked.connect(self.process_data)
-        button_draw_roi.connect(self.start_roi)
-    
-    @MyPyQtSlot("bool")
+        button_draw_roi.pressed.connect(self.start_roi)
+        button_slice_first.pressed.connect(self.change_image)
+        button_slice_last.pressed.connect(self.change_image)
+        button_slice_fwd.pressed.connect(self.change_image)
+        button_slice_bwd.pressed.connect(self.change_image)
+
+    @QTSlotExceptionRationalizer("bool")
+    def change_image(self):
+        sender_btn = self.sender().objectName()
+        num_images = len(self.images)
+        
+        if sender_btn == 'slice_first':
+            self.image_index = 0
+        elif sender_btn == 'slice_bwd':
+            ind = self.image_index - 1
+            self.image_index = ind % num_images
+        elif sender_btn == 'slice_fwd':
+            ind = self.image_index + 1
+            self.image_index = ind % num_images
+        else:
+            self.image_index = num_images - 1
+        
+        # display the slice selection label, with zero padding to keep the toolbar from shifting around
+        num, demon = str(self.image_index+1).rjust(2, '0'), str(num_images).rjust(2, '0')
+        self.slice_label.setText("{}/{}".format(num, demon))
+        self.plot_im.make_image(self.images[self.image_index])
+        
+    @QTSlotExceptionRationalizer("bool")
     def get_roi_scope(self):
         return self.combo_roi_scope.currentText()
         
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def get_roi_style(self):
         return self.combo_roi_scope.currentText()
     
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def get_relax_type(self):
         return self.combo_roi_scope.currentText()
     
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def choose_dir(self, event):
         out = QtGui.QFileDialog.getExistingDirectory(caption='MRI Data Directory')
         
@@ -205,10 +239,13 @@ class MainWindow(QtGui.QWidget):
             self.directory = out
             self.images, self.image_attributes = blood_tools.read_dicoms(out,[])
             self.plot_im.make_image(self.images[0])
+            
+            num, demon = '01', str(len(self.images)).rjust(2, '0')
+            self.slice_label.setText("{}/{}".format(num, demon))
         else: # user hit the cancel or x button to leave the dialog
             pass
     
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def start_roi(self, event):
         roi_style = self.get_roi_style()
         roi_scope = self.get_roi_scope()
@@ -216,8 +253,7 @@ class MainWindow(QtGui.QWidget):
         self.plot_im.clear_roi()
         self.plot_im.new_roi()
         
-    
-    @MyPyQtSlot("bool")
+    @QTSlotExceptionRationalizer("bool")
     def process_data(self, event):
         relaxation_type = self.get_relax_type()         
         
@@ -229,7 +265,7 @@ class MainWindow(QtGui.QWidget):
         if relaxation_type == 'T1':
             raise NotImplementedError("T1 mapping has not been implemented yet")            
         elif relaxation_type == 'T2':    
-            x, y = make_T2_fit_from_directory_and_mask(self.directory, self.images, roi)
+            x, y = make_T2_fit_from_directory_and_roi(self.directory, self.images, roi)
             self.plot_graph.axes.clear()
             self.plot_graph.axes.plot(x, y, 'ro')
             self.plot_graph.figure.canvas.draw()
@@ -239,10 +275,8 @@ class MainWindow(QtGui.QWidget):
         else:
             raise NotImplementedError("This mapping type's fitting algorithm has not been implemented yet") 
         
-                    
-def make_T2_fit_from_directory_and_mask(dicom_directory, images, roi):
-    """
-    Simplest case: makes a T2 monoexponential fit given a directory of T2 
+def make_T2_fit_from_directory_and_roi(dicom_directory, images, roi):
+    """Simplest case: makes a T2 monoexponential fit given a directory of T2 
     DICOMs and a single ROI mask.
     """
     # get T2 prep times
